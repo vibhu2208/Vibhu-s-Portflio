2a81c0f43d11c083bb2d0e069b0182b21debeb24
Vaibhav Kaushik <120114579+vibhu2208@users.noreply.github.com>
2025-03-24 02:46:06 +0530

    refactor: remove animated text from hero section for simpler design

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -16,14 +16,7 @@ const Landing = ({ children }: PropsWithChildren) => {
           </div>
           <div className="landing-info">
             <h3>A Creative</h3>
-            <h2 className="landing-info-h2">
-              <div className="landing-h2-1">Designer</div>
-              <div className="landing-h2-2">Developer</div>
-            </h2>
-            <h2>
-              <div className="landing-h2-info">Developer</div>
-              <div className="landing-h2-info-1">Designer</div>
-            </h2>
+            <h2>Developer & Designer</h2>
           </div>
         </div>
         {children}
